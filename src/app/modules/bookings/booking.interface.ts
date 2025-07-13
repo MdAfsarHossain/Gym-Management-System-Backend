9669b2f8012831c50ced98646f86156fb7ebb883
MdAfsarHossain <afsar291722@gmail.com>
2025-07-13 16:00:43 +0600

    Booking interface ts file added

--- src/app/modules/bookings/booking.interface.ts
+++ src/app/modules/bookings/booking.interface.ts
@@ -1,6 +1,6 @@
 import { Types } from "mongoose";
 
 export interface IBookingCreate {
-  classId: Types.ObjectId | any;
-  traineeId: Types.ObjectId | any;
+  classId: Types.ObjectId;
+  traineeId: Types.ObjectId;
 }
