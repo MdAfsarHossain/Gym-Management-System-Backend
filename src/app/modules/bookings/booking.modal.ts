5bdd6273af5c7b6b528c58b0ef3199423df433cd
MdAfsarHossain <afsar291722@gmail.com>
2025-07-13 16:01:12 +0600

    Booking Model ts file updated.

--- src/app/modules/bookings/booking.modal.ts
+++ src/app/modules/bookings/booking.modal.ts
@@ -1,10 +1,14 @@
-import { model, Schema, Types } from "mongoose";
+import { model, Schema } from "mongoose";
 import { IBookingCreate } from "./booking.interface";
 
 const bookingSchema = new Schema<IBookingCreate>(
   {
-    classId: { type: Types.ObjectId, ref: "ClassSchedule", required: true },
-    traineeId: { type: Types.ObjectId, ref: "User", required: true },
+    classId: {
+      type: Schema.Types.ObjectId,
+      ref: "ClassSchedule",
+      required: true,
+    },
+    traineeId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   },
   { timestamps: true, versionKey: false }
 );
