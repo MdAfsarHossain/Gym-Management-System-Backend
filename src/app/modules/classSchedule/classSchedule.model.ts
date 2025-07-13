da9a5f0f65a30d43578bd16172574bddf33ed58b
MdAfsarHossain <afsar291722@gmail.com>
2025-07-13 16:02:35 +0600

    Class Schedule Model ts file added

---
+++ src/app/modules/classSchedule/classSchedule.model.ts
@@ -0,0 +1,19 @@
+import { model, Schema } from "mongoose";
+import { IClassSchedule } from "./classSchedule.interface";
+
+// models/classSchedule.js
+const classScheduleSchema = new Schema<IClassSchedule>(
+  // const classScheduleSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    startTime: { type: Date, required: true },
+    endTime: { type: Date, required: true },
+    trainerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    bookedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const ClassSchedule = model("ClassSchedule", classScheduleSchema);
+
+export default ClassSchedule;
